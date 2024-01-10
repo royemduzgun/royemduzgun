1768d6a10d205b17aae5faeddd38e7a867e2ea12
royemduzgun <154828792+royemduzgun@users.noreply.github.com>
2024-01-10 20:22:06 +0300

    Add files via upload

---
+++ saatt.py
@@ -0,0 +1,12 @@
+import time
+import datetime
+import os
+
+while True:
+
+    clear=lambda:os.system("cls")
+    clear()
+
+    simdi=datetime.datetime.now()
+    print(simdi.strftime("%H:%M:%S"))
+    time.sleep(1)
