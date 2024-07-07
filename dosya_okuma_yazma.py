f16a814b46b370d6c986813568b8dedf87c6c741
royem duzgun <154828792+royemduzgun@users.noreply.github.com>
2024-07-07 22:57:43 +0300

    Add files via upload

---
+++ dosya_okuma_yazma.py
@@ -0,0 +1,8 @@
+saat=int(input("lütfen saatı giriniz"))
+if saat<7 or saat >7:
+    print("uyumaya devam.")
+elif saat==7:
+    print("alarm calıyor kalk!")
+else:
+    print("lütfen bir saat giriniz.")
+              
